11614018031676f6c5f5aa14ffc3f95407c078b4
puneet <puneet.yberry@gmail.com>
2018-09-07 10:42:46 +0530

    New VC With Hello World Screen.

---
+++ Swift_GitCommit/HelloWorld_VC.swift
@@ -0,0 +1,42 @@
+//
+//  HelloWorld_VC.swift
+//  Swift_GitCommit
+//
+//  Created by mac on 07/09/18.
+//  Copyright © 2018 apple. All rights reserved.
+//
+
+import UIKit
+
+class HelloWorld_VC: UIViewController {
+    @IBOutlet weak var lbl: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+  
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+        
+        
+        if (segue.identifier == "NEXT") {
+            
+            let vc = segue.destination as! HelloWorld_VC
+            vc.lbl.text = "Hello World Class"
+            
+        }
+    }
+}
--- Swift_GitCommit/ViewController.swift
+++ Swift_GitCommit/ViewController.swift
@@ -20,9 +20,13 @@ class ViewController: UIViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+   
+        
     }
 
-
+    @IBAction func btn_next(_ sender: Any) {
+        
+    }
+    
 }
 
